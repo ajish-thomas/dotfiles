07709a2e3fe422ddd609eea5deb3829d8cfdfe25
athomas <ajish.thomas@gmail.com>
2025-07-19 21:59:49 -0700

    lazy.vim based setup using mrjakob tutorials

---
+++ config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Conform will run multiple formatters sequentially
+			clang_format = {},
+			python = { "isort", "black" },
+			-- You can customize some of the format options for the filetype (:help conform.format)
+			-- rust = { "rustfmt", lsp_format = "fallback" },
+			-- Conform will run the first available formatter
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+		format_on_save = {
+			-- options to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
+}
